bb3b50af536bd0b874cb2121f9ab5faddf338320
so270744 <so270744@MI121-06>
2017-09-28 18:13:46 +0200

    test main fin de l'etape2

---
+++ mavenproject1/Panier/src/main/java/fr/ufrsciencestech/panier/App.java
@@ -0,0 +1,19 @@
+package fr.ufrsciencestech.panier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Panier monPanier = new Panier(1);
+       Orange o1 = new Orange(1.5,"Maroc");
+       
+       monPanier.ajoute(o1);
+       
+       String s=monPanier.toString();
+        System.out.println(s);
+    }
+}
