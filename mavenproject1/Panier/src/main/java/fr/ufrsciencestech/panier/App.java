36d7da1cd7342635d97d656c00c6a671f8b35c8e
so270744 <so270744@MI121-06>
2017-10-04 18:49:47 +0200

    test main fin de l'etape2

--- mavenproject1/Panier/src/main/java/fr/ufrsciencestech/panier/App.java
+++ mavenproject1/Panier/src/main/java/fr/ufrsciencestech/panier/App.java
@@ -8,10 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-       Panier monPanier = new Panier(1);
+       Panier monPanier = new Panier(5);
        Orange o1 = new Orange(1.5,"Maroc");
        
-       monPanier.ajoute(o1);
+       //monPanier.ajoute(o1);
        
        String s=monPanier.toString();
         System.out.println(s);
